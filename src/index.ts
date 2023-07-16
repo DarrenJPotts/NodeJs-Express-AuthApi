959c2dba7f00a4b6987f1f35a1f63a8806f5ba37
Darren Potts <potts.darren@gmail.com>
2023-07-16 16:57:21 +0200

    node

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import bodyParser from "body-parser";
-import compression from "compression";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import express, { Express } from "express";
+import express from "express";
 import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+import router from "./router";
 import mongoose from "mongoose";
 
-const MONGO_URL =
-  "mongodb+srv://pottsdarren:BdgRoF79oPnmp2dD@cluster0.dxxxyqq.mongodb.net/";
-
-const app: Express = express();
+const app = express();
 
 app.use(
   cors({
@@ -24,9 +23,13 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-  console.log("Server running on http://localhost:8080");
+  console.log("Server running on http://localhost:8080/");
 });
+
+const MONGO_URL = ""; // DB URI
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
